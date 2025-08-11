b8d906174efb3dee67a49562ad05df6f9ec62090
Reece Grimm <rgrimm@cs.stonybrook.edu>
2025-08-11 15:44:13 -0400

    filter options

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -344,20 +344,20 @@ export const getUserCards = async (req, res) => {
 
 
 export async function giveDailyPack() {
-  const session = await mongoose.startSession();
-  try {
+    const session = await mongoose.startSession();
+    try {
     await session.withTransaction(async () => {
-      await userModel.updateMany(
-        {packsAvailable: { $lt: 5 }},
-        { $inc: { packsAvailable: 1 } },
-        { session }
-      );
+        await userModel.updateMany(
+            {packsAvailable: { $lt: 5 }},
+            { $inc: { packsAvailable: 1 } },
+            { session }
+        );
     });
-  } catch (error) {
-    console.error('Error giving daily packs:', error);
-  } finally {
-    session.endSession();
-  }
+    } catch (error) {
+        console.error('Error giving daily packs:', error);
+    } finally {
+        session.endSession();
+    }
 }
 
 async function internalEditUser(userID, Pin, packsAvailable, session){
